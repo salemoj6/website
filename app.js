8f4e1e634c809e567aa817d511f394e680b4073f
Joseph Salemo <71265530+salemoj6@users.noreply.github.com>
2021-06-24 19:34:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var http = require('http');
-var path = require("path");
-var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var rateLimit = require("express-rate-limit");
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'jsalemo252@gmail.com',
-    pass: 'pokemonres25'
-  }
-});
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, 
@@ -33,21 +21,5 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + 'public/index.html');
 })
 
-app.get('public/action', function (req, res) {
-  var mailOptions = {
-    from: 'jsalemo252@gmail.com',
-    to: 'salemoj6@students.rowan.edu,
-    subject: 'Contact from PC maker business',
-    text: 'test'
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-});
 
 app.listen(port, () => console.log('The server running on Port '+port));
